f3e81d2740390014dcfb556de9edcd03ab12a49b
Minghao Li <minghao.li@thoughtworks.com>
2024-11-13 10:30:52 +0800

    [OO][Practice] feat: AC1

--- src/afs/training/oo/Bus.java
+++ src/afs/training/oo/Bus.java
@@ -1,4 +1,19 @@
 package afs.training.oo;
 
 public class Bus {
+
+    public static final int ACCELERATE = 5;
+    private final String name;
+
+    private int speed;
+
+    public Bus(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public void speedUp() {
+        this.speed += ACCELERATE;
+        System.out.printf("%s: speed up to %dkm/h\n", this.name, this.speed);
+    }
 }
--- src/afs/training/oo/Main.java
+++ src/afs/training/oo/Main.java
@@ -1,7 +1,14 @@
 package afs.training.oo;
 
+// AC1: A bus named “Cool Bus” with a speed of 25 km/h can accelerate(5 km/h every time) and display
+// information about the name and speed when speed up, with the following information like: “Cool Bus: speed up to 30 km/h”.
+// When get information, will print information: "This is a bus"
+// fields: name, speed
+// method: speedUp, getInformation
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bus coolBus = new Bus("Cool Bus", 25);
+        coolBus.speedUp();
     }
 }
